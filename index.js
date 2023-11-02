4857265e25c039f00ddda7cd947d04b44ee9683b
Anton Khassanov <marioanton9@gmail.com>
2023-11-02 14:33:07 -0400

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const names = ['Aki', 'Guadalupe', 'Ollie'];
+  const messages = writeCards(names);
+  console.log(messages);
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Example usage:
+  countDown(5);
+  
